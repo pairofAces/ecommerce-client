3c1c3222ffaea6ba3ab9851efdb19c7783853c18
pairofAces <karanchauhan16@gmail.com>
2020-09-08 20:45:15 -0400

    can open/close the cart

--- src/app.js
+++ src/app.js
@@ -158,6 +158,23 @@ class UI {
         cartOverlay.classList.add('transparentBcg');
         cartDOM.classList.add('showCart');
     }
+
+    setupAPP() {
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener('click', this.showCart);
+        closeCartBtn.addEventListener('click', this.hideCart)
+    }
+
+    populateCart(cart) {
+        cart.forEach(item => this.addCartItem(item));
+    }
+
+    hideCart(cart) {
+        cartOverlay.classList.remove('transparentBcg');
+        cartDOM.classList.remove('showCart');
+    }
 }
 
 //local storage class
@@ -175,12 +192,18 @@ class Storage {
     static saveCart(cart) {
         localStorage.setItem('cart', JSON.stringify(cart))
     }
+
+    static getCart() {
+        return localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')) : [] 
+    }
 }
 
 document.addEventListener('DOMContentLoaded', e => {
     const ui = new UI();
     const products = new Products();
 
+    //setup the application
+    ui.setupAPP();
     //use the class method to get the products
     products.getProducts().then(products => {
         ui.displayProducts(products)
